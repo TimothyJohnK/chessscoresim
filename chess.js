27bc3ab2e7cd06630eb2553b41c6aace0b627cb3
TimothyJohnK <Tim@VectorLab.com>
2020-07-05 16:48:45 -0500

    refactored scoreMaker for brevity

--- chess.js
+++ chess.js
@@ -1,28 +1,14 @@
-// Chess.com semantics
-// let score = document.getElementsByClassName('evaluation-bar-score');
-
-let score = document.getElementById('scorebar').textContent;
+const score = document.getElementById('scorebar').textContent;
 const button = document.getElementById('button');
 
-const scoreMaker = function (max, min) {
-  return (Math.random() * (max - min) + min).toFixed(2);
-};
-const modifier = function () {
-  let mod = Math.random() * 10;
-  if (mod % 2 > 1) {
-    return -1;
-  } else {
-    return 1;
-  }
+const scoreMaker = function () {
+  const rawScore = Math.min(Math.random() * 21, 20) - 10; // a float from -10..10
+  const truncatedScore = rawScore.toFixed(2);
+  return (score.textContent = truncatedScore);
 };
 
 button.addEventListener('click', function () {
   console.log('click firing');
-  document.getElementById('scorebar').textContent =
-    scoreMaker(0, 10) * modifier();
+  document.getElementById('scorebar').textContent = scoreMaker();
   console.log('working', scorebar.innerText);
 });
-
-// function testScore() {
-//   console.log(score.textContent);
-// }
